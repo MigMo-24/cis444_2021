f44b77d4a9db59c73d1d39ba9b2738f177a648ca
Ubuntu <ubuntu@ip-172-31-3-230.us-west-1.compute.internal>
2021-10-25 17:02:10 +0000

    Checking on the git commint -a command X

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 USER_PASSWORDS = { "cjardin": "strong password"}
 
 IMGS_URL = {
-            "DEV" : "/static",
+            "DEV" : "/static/img",
             "INT" : "https://cis-444-fall-2021.s3.us-west-2.amazonaws.com/images",
             "PRD" : "http://d2cbuxq67vowa3.cloudfront.net/images"
             }
 
-CUR_ENV = "PRD"
+CUR_ENV = "DEV"
 
 @app.route('/') #endpoint
 def index():
@@ -39,8 +39,10 @@ def backp():
 #Assigment 2
 @app.route('/ss1') #endpoint
 def ss1():
-    return render_template('server_time.html', server_time= str(datetime.datetime.now()) )
-
+    pagetitle = 'The HomePage'
+    myparagraph = 'The professor is a good person, and he will give me and A.'
+    return render_template('server_time.html',mytitle = pagetitle,mycontent =myparagraph ,server_time= str(datetime.datetime.now()) )
+#Assigment 3
 
 app.run(host='0.0.0.0', port=80)
 
