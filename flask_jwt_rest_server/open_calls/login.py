a306583f9ff1558c38139282cfb116009898698f
MigMo-24 <ubuntu@ip-172-31-3-230.us-west-1.compute.internal>
2021-11-16 17:01:59 +0000

    ading true to login.py

--- flask_jwt_rest_server/open_calls/login.py
+++ flask_jwt_rest_server/open_calls/login.py
@@ -1,7 +1,7 @@
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from tools.token_tools import create_token
-
+import bcrypt
 from tools.logging import logger
 
 def handle_request():
@@ -9,11 +9,15 @@ def handle_request():
     #use data here to auth the user
 
     password_from_user_form = request.form['password']
+    print("form password is: ", password_from_user_form)
     user = {
             "sub" : request.form['firstname'] #sub is used by pyJwt as the owner of the token
             }
+    username = request.form['firstname']
+    print("form the user is: ", username)
+    
     if not user:
         return json_response(status_=401, message = 'Invalid credentials', authenticated =  False )
 
-    return json_response( token = create_token(user) , authenticated = False)
+    return json_response( token = create_token(user) , authenticated = True)
 
