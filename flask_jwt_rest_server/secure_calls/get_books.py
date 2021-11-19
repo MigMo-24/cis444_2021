c71ef5437b154f4aa8315b037a5040e750e2b843
MigMo-24 <ubuntu@ip-172-31-3-230.us-west-1.compute.internal>
2021-11-19 17:26:09 +0000

    added buyclick

--- flask_jwt_rest_server/secure_calls/get_books.py
+++ flask_jwt_rest_server/secure_calls/get_books.py
@@ -5,16 +5,31 @@ from tools.token_tools import create_token
 from tools.logging import logger
 
 def handle_request():
-    logger.debug("Get Books Handle Request") 
-    id =  request.form['book_id']
-    print(id)
-
-    cur = g.db.cursor()
-    cur.execute("Select * from _book")
-    books = cur.fetchall()
-    bookList = []
-    for r in books:
-        bookList.append(r)
-    print("Books have ", books)
+    logger.debug("Get Books Handle Request")
+    book_id = 0
+    book_id = request.args.get('book_id')
+    print(book_id)
+    isEmpty = bool(book_id)
+    print (isEmpty)
+    if isEmpty == False :
+       cur = g.db.cursor()
+       cur.execute("Select * from _book")
+       books = cur.fetchall()
+       bookList = []
+       for r in books:
+           bookList.append(r)
+           print("Books have ", books)
+    else:
+        cur = g.db.cursor()
+        cur.execute(f"select * from _book where book_id = '{book_id}';")
+        books = cur.fetchall()
+        cur.execute(f"select * from _book where book_id = '{book_id}';") 
+        bookName  = cur.fetchone()[1]
+        cur.execute(f"select * from _book where book_id = '{book_id}';") 
+        price = cur.fetchone()[3]
+        print("The book selected is: ",bookName, price)
+        cur.execute("Insert into purchase(title, price)values('%s', '%s');" %(bookName, price))
+        g.db.commit()
+        
     return json_response( token = create_token(  g.jwt_data ), data = books)
 
