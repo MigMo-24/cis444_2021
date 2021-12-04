5811b9b0c0d6d3232c2eb6779a265ca92b7d31e4
MigMo-24 <ubuntu@ip-172-31-3-230.us-west-1.compute.internal>
2021-12-04 20:06:00 +0000

    chat working

--- myChat/main.py
+++ myChat/main.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template,request
 from flask_socketio import SocketIO
+from flask_json import FlaskJSON, JsonError, json_response, as_json
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
@@ -18,4 +20,4 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug =True, host='0.0.0.0', port=80)
